f024b09d8444eafeff804507b51945ee5bae03a9
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-09-25 22:32:11 +0700

    Update UserController.java

--- backend/MealPlan/src/main/java/Project01/AWS/MealPlan/controller/UserController.java
+++ backend/MealPlan/src/main/java/Project01/AWS/MealPlan/controller/UserController.java
@@ -11,25 +11,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/users")
-@RestController
 public class UserController {
-    private final UserServiceImpl userService;
-
-    public UserController(UserServiceImpl userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/me")
-    public ResponseEntity<User> authenticatedUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User currentUser = (User) authentication.getPrincipal();
-        return ResponseEntity.ok(currentUser);
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userService.getUsers();
-        return ResponseEntity.ok(users);
-    }
 }
